96262fbca3310290d0083f6186bd0767bef0fc0a
MutharasuNCOG <mutharasu@ncog.earth>
2025-05-03 15:21:35 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Iframe Sample APP</h1>
       <div className="card">
-      <iframe src="https://5cbc-14-102-2-70.ngrok-free.app/dform/223738666539643836383538303822/short" width="1000px" height="1000px" style={{border:"none"}} allowFullScreen loading="lazy"></iframe>      </div>
+      <iframe src="https://5cbc-14-102-2-70.ngrok-free.app/dform/226366623831346138333338343222/short" width="1000px" height="1000px" style={{border:"none"}} allowFullScreen loading="lazy"></iframe>
+      </div>
     </>
   )
 }
