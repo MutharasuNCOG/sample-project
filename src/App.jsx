f41b48b481c69e6bd030062e139b623362bb923e
MutharasuNCOG <mutharasu@ncog.earth>
2025-05-07 13:51:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <h1>Iframe Sample APP</h1>
       <div className="card">
       <iframe src="https://ecbd-14-102-2-70.ngrok-free.app/dform/226564653562306263303739323022/short" width="1200px" height="1200px"  style={{border:"none"}} allowFullScreen loading="lazy"></iframe>
+      
       </div>
     </>
   )
